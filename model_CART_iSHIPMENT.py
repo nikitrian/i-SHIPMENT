84c53f8025eda67a7544bf3a28290628dbe2033c
Triantafyllou <nt320@ic.ac.uk>
2025-03-19 13:33:21 +0000

    latest code version

--- model_CART_iSHIPMENT.py
+++ model_CART_iSHIPMENT.py
@@ -1,12 +1,10 @@
 from pyomo.environ import *
-from pyomo.common.timing import TicTocTimer, report_timing
+from pyomo.common.timing import TicTocTimer
 from time import process_time
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import random 
 model = AbstractModel()
-data = 'Data50_profileA.dat'
+data = 'Data200_profileA.dat'
 t1_start = process_time()
 
 # SETS
@@ -35,14 +33,17 @@ model.FMIN = Param() # Minimum flow
 model.TAD = Param(within=NonNegativeReals) # Duration of administration
 model.TLS = Param(within=NonNegativeReals) # Duration of leukapheresis
 model.TMFE = Param(default=7) # Duration of manufacturing excluding QC
-
+model.TQC = Param(default=7) # Duration of QC
+model.C_material = Param(default=10476) # Materials cost per therapy
+model.CQC= Param(default=9312) # QC cost per therapy
+model.ND = Param(default = 18) # Maximum turnaround (vein-to-vein) time
 
 # Binary VARIABLES
 model.E1 = Var(model.m, within=Binary) # 1 if manufacturing facility m is established
 model.X1 = Var(model.c, model.m, within=Binary) # 1 if a match between LS site c and MS site m is established
-model.X3 = Var(model.m, model.h, within=Binary) # 1 if a match between MS site m and a hospital h is established
+model.X2 = Var(model.m, model.h, within=Binary) # 1 if a match between MS site m and a hospital h is established
 model.Y1 = Var(model.p, model.c, model.m, model.j, model.t, within=Binary) # 1 if a sample p is transferred from a LS site c to a MS site m via mode j at time t
-model.Y3 = Var(model.p, model.m, model.h, model.j, model.t, within=Binary) # 1 if a sample p is transferred from a MS site m to a hospital h via mode j at time t
+model.Y2 = Var(model.p, model.m, model.h, model.j, model.t, within=Binary) # 1 if a sample p is transferred from a MS site m to a hospital h via mode j at time t
 
 # Integer variables
 model.INH = Var(model.p, model.h, model.t, within=NonNegativeIntegers) # Therapy p arriving at hospital h at time t
@@ -71,40 +72,33 @@ model.CTT = Var(model.p) # Completion time of treatment for patient p
 
 
 # OBJECTIVE FUNCTION
-# 10476 is the material cost, 9312 is the QC cost
 def obj_rule(model):
-    return sum( model.CTM[p]for p in model.p )+ sum( model.TTC[p] for p in model.p ) + (10476+9312)*len(model.p)
+    return sum( model.CTM[p]for p in model.p )+ sum( model.TTC[p] for p in model.p ) + (model.C_material + model.CQC)* len(model.p)
 model.obj = Objective( rule=obj_rule )
 
 
 # CONSTRAINTS
 # Manufacturing cost
-# C1
-# base case: 58000 $ 80% utilisation
 def C1_rule(model,p):
     return model.CTM[p] == sum((model.E1[m]*(model.CIM[m]+model.CVM[m]))*len(model.t)/len(model.p) for m in model.m)
 model.C1 = Constraint(model.p, rule=C1_rule)
 
+# Transportation cost
+def C2_rule(model,p):
+    return model.TTC[p] == sum(model.Y1[p,c,m,j,t]*model.U1[c,m,j] for c in model.c for m in model.m for j in model.j for t in model.t) + sum(model.Y2[p,m,h,j,t]*model.U3[m,h,j] for m in model.m for h in model.h for j in model.j for t in model.t)
+model.C2 = Constraint(model.p, rule=C2_rule)
 
-#RATIOEQ
+#The percentage of utilisation of facility m at time t
 def RATIOEQ_rule(model,m,t):
     return model.RATIO[m,t] == sum(model.DURV[p,m,t]/model.FCAP[m] for p in model.p)
 model.RATIOEQ = Constraint(model.m, model.t, rule=RATIOEQ_rule)
 
-
-#MSBnew
 def MSBnew_rule(model,p,m,t):
      return model.DURV[p,m,t] == sum(model.INM[p,m,tt-1]-model.OUTM[p,m,tt] for tt in model.tt if tt<=t and tt>1) + model.OUTM[p,m,t] 
 model.MSBnew = Constraint(model.p, model.m, model.t, rule=MSBnew_rule)
 
 
-# Transport cost
-#C2
-def C2_rule(model,p):
-    return model.TTC[p] == sum(model.Y1[p,c,m,j,t]*model.U1[c,m,j] for c in model.c for m in model.m for j in model.j for t in model.t) + sum(model.Y3[p,m,h,j,t]*model.U3[m,h,j] for m in model.m for h in model.h for j in model.j for t in model.t)
-model.C2 = Constraint(model.p, rule=C2_rule)
-
-
+# Material flow balances
 #MSB1
 def MSB1_rule(model,p,c,t,tt):
     if tt == t + model.TLS:
@@ -113,6 +107,23 @@ def MSB1_rule(model,p,c,t,tt):
         return Constraint.Skip
 model.MSB1 = Constraint(model.p, model.c, model.t, model.tt, rule=MSB1_rule)
 
+#MSB3
+def MSB3_rule(model,p,c,m,j,t,tt):
+    if tt == t + model.TT1[j]:
+        return model.LSR[p,c,m,j,t] == model.LSA[p,c,m,j,tt]
+    else:
+        return Constraint.Skip
+model.MSB3 = Constraint(model.p, model.c, model.m, model.j, model.t, model.tt, rule=MSB3_rule)
+
+#MSB7
+def MSB7_rule(model,p,c,t):
+    return model.OUTC[p,c,t] == sum(model.LSR[p,c,m,j,t] for m in model.m for j in model.j)
+model.MSB7 = Constraint(model.p, model.c, model.t, rule=MSB7_rule)
+
+#MSB5
+def MSB5_rule(model,p,m,t):
+    return model.INM[p,m,t] == sum(model.LSA[p,c,m,j,t] for c in model.c for j in model.j)
+model.MSB5 = Constraint(model.p, model.m, model.t, rule=MSB5_rule)
 
 #MSB2
 def MSB2_rule(model,p,m,t,tt):
@@ -122,23 +133,13 @@ def MSB2_rule(model,p,m,t,tt):
         return Constraint.Skip
 model.MSB2 = Constraint(model.p, model.m, model.t, model.tt, rule=MSB2_rule)
 
-
 #MSB8
 def MSB8_rule(model,p,m,t,tt):
-    if tt == t + 7:
+    if tt == t + model.TQC:
         return model.OUTM[p,m,t] == sum(model.MSO[p,m,h,j,tt] for h in model.h for j in model.j)
     else:
         return Constraint.Skip
 model.MSB8 = Constraint(model.p, model.m, model.t, model.tt, rule=MSB8_rule)
-
-
-#MSB3
-def MSB3_rule(model,p,c,m,j,t,tt):
-    if tt == t + model.TT1[j]:
-        return model.LSR[p,c,m,j,t] == model.LSA[p,c,m,j,tt]
-    else:
-        return Constraint.Skip
-model.MSB3 = Constraint(model.p, model.c, model.m, model.j, model.t, model.tt, rule=MSB3_rule)
 
 
 #MSB4
@@ -149,46 +150,31 @@ def MSB4_rule(model,p,m,h,j,t,tt):
         return Constraint.Skip
 model.MSB4 = Constraint(model.p, model.m, model.h, model.j, model.t, model.tt, rule=MSB4_rule)
 
-
-#MSB5
-def MSB5_rule(model,p,m,t):
-    return model.INM[p,m,t] == sum(model.LSA[p,c,m,j,t] for c in model.c for j in model.j)
-model.MSB5 = Constraint(model.p, model.m, model.t, rule=MSB5_rule)
-
-
 #MSB6
 def MSB6_rule(model,p,h,t):
     return model.INH[p,h,t] == sum(model.FTD[p,m,h,j,t] for m in model.m for j in model.j)
 model.MSB6 = Constraint(model.p, model.h, model.t, rule=MSB6_rule)
 
 
-#MSB7
-def MSB7_rule(model,p,c,t):
-    return model.OUTC[p,c,t] == sum(model.LSR[p,c,m,j,t] for m in model.m for j in model.j)
-model.MSB7 = Constraint(model.p, model.c, model.t, rule=MSB7_rule)
-
-
-# Capacity equation
-#CAP1
+# Capacity constraints
 def CAP1_rule(model,m,t):
     return model.CAP[m,t] == model.FCAP[m]-sum(model.INM[p,m,tt] for p in model.p for tt in model.tt if tt<t and tt>=t-model.TMFE)
 model.CAP1 = Constraint(model.m, model.t, rule=CAP1_rule)
 
 
-# Capacity constraint
-#CAPCON1
 def CAPCON1_rule(model,m,t):
     return sum(model.INM[p,m,t] for p in model.p)-sum(model.OUTM[p,m,t] for p in model.p) <= model.CAP[m,t]
 model.CAPCON1 = Constraint(model.m, model.t, rule=CAPCON1_rule)
 
 
-# Constraints to ensure that no matches are established with non-existent facilities
+# NETWORK STRUCTURE CONSTRAINTS
+# Constraint to allow the establishment of up to 2 manufacturing facilities (centralized network). Relax this for the investigation of decentralization
 #CON1
 def CON1_rule(model):
     return sum(model.E1[m] for m in model.m) <= 2
 model.CON1 = Constraint(rule=CON1_rule)
 
-
+# Constraints to ensure that no matches are established with non-existent facilities
 #CON2
 def CON2_rule(model,c,m):
     return model.X1[c,m] <= model.E1[m]
@@ -197,22 +183,21 @@ model.CON2 = Constraint(model.c, model.m, rule=CON2_rule)
 
 #CON3
 def CON3_rule(model,m,h):
-    return model.X3[m,h] <= model.E1[m]
+    return model.X2[m,h] <= model.E1[m]
 model.CON3 = Constraint(model.m, model.h, rule=CON3_rule)
 
-
+# Logical constraints for transportation flows
 #CON4
 def CON4_rule(model,p,c,m,j,t):
     return model.Y1[p,c,m,j,t] <= model.X1[c,m]
 model.CON4 = Constraint(model.p, model.c, model.m, model.j, model.t, rule=CON4_rule)
 
-
 #CON5
 def CON5_rule(model,p,m,h,j,t):
-    return model.Y3[p,m,h,j,t] <= model.X3[m,h]
+    return model.Y2[p,m,h,j,t] <= model.X2[m,h]
 model.CON5 = Constraint(model.p, model.m, model.h, model.j, model.t, rule=CON5_rule)
 
-
+# Ensure that only one transport mode is selected for every therapy p at every journey can be selected
 #CON6
 def CON6_rule(model,p):
     return sum(model.Y1[p,c,m,j,t] for c in model.c for m in model.m for j in model.j for t in model.t) == 1
@@ -221,7 +206,7 @@ model.CON6 = Constraint(model.p, rule=CON6_rule)
 
 #CON7
 def CON7_rule(model,p):
-    return sum(model.Y3[p,m,h,j,t] for m in model.m for h in model.h for j in model.j for t in model.t) == 1
+    return sum(model.Y2[p,m,h,j,t] for m in model.m for h in model.h for j in model.j for t in model.t) == 1
 model.CON7 = Constraint(model.p, rule=CON7_rule)
 
 
@@ -233,6 +218,7 @@ model.DEM = Constraint(rule=DEM_rule)
 
 
 # Flow constraints
+# These ensure that a minimum and maximum flow of material exists for a transportation link to be established
 #CON8
 def CON8_rule(model,p,c,m,j,t):
     return model.LSR[p,c,m,j,t] >= model.Y1[p,c,m,j,t]*model.FMIN
@@ -247,74 +233,74 @@ model.CON9 = Constraint(model.p, model.c, model.m, model.j, model.t, rule=CON9_r
 
 #CON10
 def CON10_rule(model,p,m,h,j,t):
-    return model.MSO[p,m,h,j,t] >= model.Y3[p,m,h,j,t]*model.FMIN
+    return model.MSO[p,m,h,j,t] >= model.Y2[p,m,h,j,t]*model.FMIN
 model.CON10 = Constraint(model.p, model.m, model.h, model.j, model.t, rule=CON10_rule)
 
 
 #CON11
 def CON11_rule(model,p,m,h,j,t):
-    return model.MSO[p,m,h,j,t] <= model.Y3[p,m,h,j,t]*model.FMAX
+    return model.MSO[p,m,h,j,t] <= model.Y2[p,m,h,j,t]*model.FMAX
 model.CON11 = Constraint(model.p, model.m, model.h, model.j, model.t, rule=CON11_rule)
 
-
+# These constraints make sure that a match is only made between a leukapheresis site c and its corresponding co-located hospital h
 #CON12
 def CON12_rule(model,p):
-    return sum(model.Y3[p,m,'h1',j,t] for m in model.m for j in model.j for t in model.t) == sum(model.INC[p,'c1',t] for t in model.t)
+    return sum(model.Y2[p,m,'h1',j,t] for m in model.m for j in model.j for t in model.t) == sum(model.INC[p,'c1',t] for t in model.t)
 model.CON12 = Constraint(model.p, rule=CON12_rule)
 
 
 #CON13
 def CON13_rule(model,p):
-    return sum(model.Y3[p,m,'h2',j,t] for m in model.m for j in model.j for t in model.t) == sum(model.INC[p,'c2',t] for t in model.t)
+    return sum(model.Y2[p,m,'h2',j,t] for m in model.m for j in model.j for t in model.t) == sum(model.INC[p,'c2',t] for t in model.t)
 model.CON13 = Constraint(model.p, rule=CON13_rule)
 
 
 #CON14
 def CON14_rule(model,p):
-    return sum(model.Y3[p,m,'h3',j,t] for m in model.m for j in model.j for t in model.t) == sum(model.INC[p,'c3',t] for t in model.t)
+    return sum(model.Y2[p,m,'h3',j,t] for m in model.m for j in model.j for t in model.t) == sum(model.INC[p,'c3',t] for t in model.t)
 model.CON14 = Constraint(model.p, rule=CON14_rule)
 
 
 #CON15
 def CON15_rule(model,p):
-    return sum(model.Y3[p,m,'h4',j,t] for m in model.m for j in model.j for t in model.t) == sum(model.INC[p,'c4',t] for t in model.t)
+    return sum(model.Y2[p,m,'h4',j,t] for m in model.m for j in model.j for t in model.t) == sum(model.INC[p,'c4',t] for t in model.t)
 model.CON15 = Constraint(model.p, rule=CON15_rule)
 
 
-#TCON
+# Time constraints
+#Maximum turnaround time
 def TCON_rule(model,p):
-    return model.TRT[p] <= 18
+    return model.TRT[p] <= model.ND
 model.TCON = Constraint(model.p, rule=TCON_rule)
 
 
-#START
+#START time of leukapheresis
 def START_rule(model,p):
     return model.STT[p] == sum(model.INC[p,c,t]*t for c in model.c for t in model.t)
 model.START = Constraint(model.p, rule=START_rule)
 
 
-#END
+#END time of therapy administration
 def END_rule(model,p):
     return model.CTT[p] == sum(model.INH[p,h,t]*t for h in model.h for t in model.t)
 model.END = Constraint(model.p, rule=END_rule)
 
+# Makes sure that the time point a patient checks into a leukapheresis site chronologically precedes the time point the corresponding therapy p is delivered to the hospital 
+def TSEQ_rule(model,p):
+    return model.STT[p] <= model.CTT[p]
+model.TSEQ = Constraint(model.p, rule=TSEQ_rule)
 
-#TIME
+
+# Total turnaround time
 def TIME_rule(model,p):
     return model.TRT[p] == model.CTT[p] - model.STT[p]
 model.TIME = Constraint(model.p, rule=TIME_rule)
 
 
-#ATIME
+#Average turnaround time
 def ATIME_rule(model):
     return model.ATRT == sum(model.TRT[p] for p in model.p)/len(model.p)
 model.ATIME = Constraint(rule=ATIME_rule)
-
-
-#TSEQ
-def TSEQ_rule(model,p):
-    return model.STT[p] <= model.CTT[p]
-model.TSEQ = Constraint(model.p, rule=TSEQ_rule)
 
 
 
@@ -331,9 +317,10 @@ timer.toc('Built model')
 
 print('-----------------------------------------------Solving model------------------------------------------------------')
 print('------------------------------------------------------------------------------------------------------------------')
-opt = SolverFactory('cplex')
+opt = SolverFactory('gurobi')
 myoptions = dict()
-myoptions['log'] = '200_profileA.log'
+myoptions = {'OutputFlag': 1}  #Gurobi log
+
 #myoptions['timelimit'] = 2
 results = opt.solve(instance, options=myoptions, tee=True) # solves and updates instance
 timer.toc('Time to solve')
@@ -348,7 +335,8 @@ print('------------------------------------------------RATIO(m,t)---------------
 print('-----------------------------------------------------------------------------------------------------------------------')
 for t in instance.t:
     for m in instance.m:
-        print('The percentage of utilisation of MS site', m, 'at time','t{}'.format(t), 'is', value(instance.RATIO[m,t])*100,'%')
+        if value(instance.RATIO[m,t])*100 > 1e-3:
+            print('The percentage of utilisation of MS site', m, 'at time','t{}'.format(t), 'is', value(instance.RATIO[m,t])*100,'%')
 print('-----------------------------------------------------------------------------------------------------------------------')
 print('-----------------------------------------------------------------------------------------------------------------------')
 print('-----------------------------------------------------------------------------------------------------------------------')
@@ -534,7 +522,7 @@ for c in instance.c:
 
 for m in instance.m:
     for h in instance.h:
-        if (value(instance.X3[m,h]))!=0:
+        if (value(instance.X2[m,h]))!=0:
             plt.plot([manufacturingX[Nm-instance.m.ord(m)],hospitalX[Nh-instance.h.ord(h)]],[manufacturingY[Nm-instance.m.ord(m)],hospitalY[Nh-instance.h.ord(h)]],color='darkmagenta')
             
 
